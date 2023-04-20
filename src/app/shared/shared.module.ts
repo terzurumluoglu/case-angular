ac9a30877c00298131ff240cd03b072231aee00f
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-20 23:17:12 +0300

    Form component was created to share

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormComponent } from './components/form/form.component';
+import { FormService } from './services/form.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormComponent
+  ],
   imports: [
     CommonModule
   ],
+  exports: [
+    FormComponent,
+  ]
 })
 export class SharedModule { }
