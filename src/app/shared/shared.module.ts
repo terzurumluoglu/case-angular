ef92dcf5b92a41400a3b095433d418441323eda3
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 00:02:40 +0300

    reactive forms module was added!

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './components/form/form.component';
 import { FormService } from './services/form.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     FormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    FormService
   ],
   exports: [
     FormComponent,
