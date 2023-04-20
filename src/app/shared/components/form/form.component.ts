7a475768b6b4b88c9f885c5b0631dccbfdecf7b7
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 00:04:34 +0300

    dynamic form was created under the shared

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormService } from '../../services/form.service';
+import { FormGroup } from '@angular/forms';
+import { IField } from 'src/app/model/IField';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
 
-  constructor() { }
+  @Input() formName?: string;
+  
+  @Input() set fields(val: IField[]) {
+    this.formFields = val;
+    this.createForm(val);
+  }
 
-  ngOnInit(): void {
+  @Output() sendFormData = new EventEmitter();
+
+  form!: FormGroup;
+  formFields!: IField[];
+
+  constructor(
+    private formService: FormService
+  ) { }
+
+  createForm(fields: IField[]) {
+    this.form = this.formService.createReactiveForm(fields);
+  }
+
+  submit() {
+    this.sendFormData.emit(this.form);
   }
 
 }
