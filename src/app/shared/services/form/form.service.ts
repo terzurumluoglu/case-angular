4f930ffb42860f63fffce538c281a081eed5b4ba
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 22:31:10 +0300

    changed folder

--- src/app/shared/services/form/form.service.ts
+++ src/app/shared/services/form/form.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import { IField } from '../../model/IField';
+import { IField } from '../../../model/IField';
 
 @Injectable({
   providedIn: 'root'
