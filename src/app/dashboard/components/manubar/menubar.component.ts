d3eda544f98eae03cd83db9dac6275dd83112588
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 23:29:23 +0300

    menubar and dashboard design changed

--- src/app/dashboard/components/manubar/menubar.component.ts
+++ src/app/dashboard/components/manubar/menubar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth/auth.service';
 import { IIdentity } from 'src/app/model';
+import { ACCOUNT_MENUS, MENUS } from './menubar.mock';
 
 @Component({
   selector: 'app-menubar',
@@ -10,13 +11,24 @@ import { IIdentity } from 'src/app/model';
 export class MenubarComponent implements OnInit {
 
   identity?: IIdentity;
+  menus: any[] = MENUS;
+  accountMenus: any = ACCOUNT_MENUS;
 
   constructor(
     private authService: AuthService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.identity = this.authService.userValue;
+    this.accountMenus.name = this.accountMenus.name.split('{{USER}}').join(`${this.identity?.name} ${this.identity?.surname}`);
+    this.accountMenus.subMenus.forEach((subMenu: any) => {
+      if (subMenu.name === 'logout') {
+        subMenu.fn = () => {
+          this.logout();
+        }
+      }
+    });
   }
 
   logout() {
---
+++ src/app/dashboard/components/manubar/menubar.mock.ts
@@ -0,0 +1,38 @@
+export const MENUS: any[] = [
+    {
+        name: 'Öğrenci İşlemleri',
+        route: 'student'
+    },
+    {
+        name: 'Müfredat',
+        route: 'curriculum'
+    },
+    {
+        name: 'Ders İşlemleri',
+        route: 'course'
+    }
+];
+
+export const ACCOUNT_MENUS: any = {
+    name: '{{USER}}',
+    subMenus: [
+        {
+            name: 'Kimlik Bilgileri',
+            route: 'identity'
+        },
+        {
+            name: 'İletişim Bilgileri',
+            route: 'contact'
+        },
+        {
+            name: 'Ders İşlemleri',
+            route: 'course'
+        },
+        {
+            name: 'logout',
+            fn: () => {
+                console.log('HOP');
+            }
+        }
+    ]
+}
