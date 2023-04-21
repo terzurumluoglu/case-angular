4ac773d3c2bdca8017f8e31d19a7849e9e422776
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 20:56:21 +0300

    auth guard created

---
+++ src/app/auth/guards/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const user = this.authService.userValue;
+    if (user) {
+      // logged in so return true
+      return true;
+    } else {
+      // not logged in so redirect to login page with the return url
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
+    }
+  }
+  
+}
