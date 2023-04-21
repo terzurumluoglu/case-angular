5d331812ba819d09225f676f81ea9439121c6745
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 23:46:08 +0300

    added new component and their routing

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { IdentityComponent } from './components/identity/identity.component';
+import { StudentComponent } from './components/student/student.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { CourseComponent } from './components/course/course.component';
 
 const routes: Routes = [
   {
@@ -11,6 +15,22 @@ const routes: Routes = [
   {
     path: 'identity',
     component: IdentityComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'curriculum',
+    component: CurriculumComponent
+  },
+  {
+    path: 'course',
+    component: CourseComponent
   }
 ];
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { MenubarComponent } from './components/manubar/menubar.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
 import { DashboardComponent } from './dashboard.component';
 import { IdentityComponent } from './components/identity/identity.component';
+import { StudentComponent } from './components/student/student.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { CourseComponent } from './components/course/course.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 @NgModule({
   declarations: [
     MenubarComponent,
     DashboardComponent,
-    IdentityComponent
+    IdentityComponent,
+    StudentComponent,
+    CurriculumComponent,
+    CourseComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
