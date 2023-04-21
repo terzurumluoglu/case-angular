75bf085b254f3e98d44df9e1125fa89c5c5e4655
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 23:28:16 +0300

    identity service added!

---
+++ src/app/shared/services/identity/identity.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom, map } from 'rxjs';
+import { IIdentity } from 'src/app/model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IdentityService {
+
+  identityPath: string = 'identity.json';
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.identityPath = environment.apiUrl + this.identityPath;
+  }
+
+  getAllIdentities() {
+    return lastValueFrom(this.http.get(this.identityPath));
+  }
+
+  getIdentityById(id: number): Promise<IIdentity | undefined> {
+    return lastValueFrom(this.http.get<IIdentity[]>(this.identityPath).pipe(
+      map((identities: IIdentity[]) => {
+        return identities.find(identity => identity.id === id);
+      })
+    ));
+  }
+
+}
