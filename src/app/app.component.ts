d7f4989da8e6ee92acd98861327d405670dd92e2
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-21 23:28:41 +0300

    initial user loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'case-angular';
+
+  constructor(private auth: AuthService) {
+    let identity = localStorage.getItem('identity');
+    if (!!identity && !auth.userValue) {
+      auth.userSubject$.next(JSON.parse(identity)); 
+    }
+  }
 }
