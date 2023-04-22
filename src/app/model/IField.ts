b1ac87cecb694e679ae31772c6e57cae4e81d4e9
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-23 00:31:05 +0300

    added som necessery prop

--- src/app/model/IField.ts
+++ src/app/model/IField.ts
@@ -4,5 +4,6 @@ export interface IField {
     name: string;
     initialValue: any;
     validators: ValidatorFn[];
+    disabled: boolean;
     type: 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week';
 }
