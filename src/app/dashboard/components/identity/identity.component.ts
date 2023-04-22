08f96d553823b50ffa09d7d77e9488c5f8294912
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-23 00:37:33 +0300

    identity done

--- src/app/dashboard/components/identity/identity.component.ts
+++ src/app/dashboard/components/identity/identity.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { AuthService } from 'src/app/auth/services/auth/auth.service';
+import { IField, IIdentity } from 'src/app/model';
 
 @Component({
   selector: 'app-identity',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentityComponent implements OnInit {
 
-  constructor() { }
+  fields: IField[] = [
+    {
+      name: 'tc',
+      disabled: true,
+      initialValue: null,
+      type: 'text',
+      validators: [Validators.required]
+    },
+    {
+      name: 'name',
+      disabled: true,
+      initialValue: null,
+      type: 'text',
+      validators: [Validators.required]
+    },
+    {
+      name: 'surname',
+      disabled: true,
+      initialValue: null,
+      type: 'text',
+      validators: [Validators.required]
+    },
+    {
+      name: 'bornCity',
+      disabled: true,
+      initialValue: null,
+      type: 'text',
+      validators: [Validators.required]
+    },
+    {
+      name: 'bornDate',
+      disabled: true,
+      initialValue: null,
+      type: 'text',
+      validators: [Validators.required]
+    },
+  ];
+
+  identity: IIdentity | any;
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.identity = this.authService.userValue;
+    this.fields.forEach(item => {
+      item.initialValue = this.identity[item.name];
+    })
   }
 
 }
